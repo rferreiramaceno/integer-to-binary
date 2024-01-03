5f4924a423e4b2a525f1ae384291a21cdbb305f2
rferreiramaceno <rferreiramaceno@gmail.com>
2024-01-02 21:58:58 -0700

    Wrote main function. Program displays output correctly.

--- display_binary.py
+++ display_binary.py
@@ -7,3 +7,17 @@ def display_binary(num):
         num = int(num / 2)
     binary = binary[::-1]
     return binary
+
+def main():
+    x = input("Enter a number to convert it to binary. ")
+
+    while not x.strip("-").isnumeric():
+        print("You didn't enter a valid integer!")
+        x = input("Enter a number to convert it to binary. ")
+    if x[0] == "-":
+        x = x[1:]
+        print("-" + display_binary_num(int(x)))
+    else:
+        print(display_binary_num(int(x)))
+
+main()
